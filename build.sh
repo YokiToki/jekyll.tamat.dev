744a04db36eb9f5d0303adda62a1386bc183cf80
tamat <libastral.so@yandex.ru>
2021-04-07 15:02:29 +0700

    Plugin

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ rsync -av --progress _site/ _site-multilanguage/ --exclude en
 git checkout languge/en
 mkdir -p _site-multilanguage/en
 jekyll build
-rsync -av --progress _site/en _site-multilanguage/en
+rsync -av --progress _site/en _site-multilanguage
 
 git checkout master
